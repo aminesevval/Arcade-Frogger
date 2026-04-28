d871db7ec86dc1552b993fe3e0e980bf651058e3
Amine Şevval Yorulmaz <amine.yorulmaz@istun.edu.tr>
2026-04-28 23:49:09 +0300

    1. Hafta: Kurbaga hareketi ve gorseli tamamlandi

--- main.c
+++ main.c
@@ -1,108 +1,73 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic 3d example
-*
-*   Welcome to raylib!
-*
-*   To compile example, just press F5.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2026 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
-//----------------------------------------------------------------------------------
-// Global Variables Definition (local to this module)
-//----------------------------------------------------------------------------------
-Camera camera = { 0 };
-Vector3 cubePosition = { 0 };
-
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
-static void UpdateDrawFrame(void);          // Update and draw one frame
-
-//----------------------------------------------------------------------------------
-// Program main entry point
-//----------------------------------------------------------------------------------
-int main()
+int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    // --- 1. AYARLAR VE PENCERE AÇILIŞI ---
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "raylib");
+    // Pencereyi oluşturuyoruz
+    InitWindow(screenWidth, screenHeight, "Arcade Frogger - Amine Sevval");
 
-    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
+    // --- 2. GÖRSEL YÜKLEME ---
+    // Dikkat: assets klasöründe frog.png dosyanın olduğundan emin ol!
+    Texture2D frogImage = LoadTexture("assets/frog.png");
 
-    //--------------------------------------------------------------------------------------
+    // Kurbağanın başlangıç pozisyonu (Ekranın alt-orta kısmı)
+    // Resmin boyutlarını hesaba katarak tam merkeze oturtuyoruz
+    Vector2 frogPos = { 
+        (float)screenWidth/2 - (float)frogImage.width/2, 
+        (float)screenHeight - (float)frogImage.height - 20 
+    };
 
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-#else
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    float speed = 5.0f; // Hareket hızı
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    SetTargetFPS(60); // Oyunun saniyede 60 kare çalışmasını sağlar
+
+    // --- 3. ANA OYUN DÖNGÜSÜ ---
+    while (!WindowShouldClose())
     {
-        UpdateDrawFrame();
-    }
-#endif
+        // --- GÜNCELLEME (HAREKET) ---
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) frogPos.x += speed;
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) frogPos.x -= speed;
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) frogPos.y -= speed;
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) frogPos.y += speed;
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();                  // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+        // --- SINIR KONTROLLERİ (Kaçış Engelleme) ---
+        // Sol sınır
+        if (frogPos.x < 0) frogPos.x = 0;
+        
+        // Sağ sınır (Ekran genişliğinden kurbağanın genişliğini çıkarıyoruz)
+        if (frogPos.x + frogImage.width > screenWidth) {
+            frogPos.x = screenWidth - frogImage.width;
+        }
+
+        // Üst sınır
+        if (frogPos.y < 0) frogPos.y = 0;
+
+        // Alt sınır (Ekran yüksekliğinden kurbağanın yüksekliğini çıkarıyoruz)
+        if (frogPos.y + frogImage.height > screenHeight) {
+            frogPos.y = screenHeight - frogImage.height;
+        }
+
+        // --- ÇİZİM AŞAMASI ---
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE); // Ekranı her karede temizle (Beyaz arka plan)
+            
+            // Kurbağayı yeni pozisyonunda çiz
+            DrawTextureV(frogImage, frogPos, WHITE);
+            
+            // Yardımcı metinler
+            DrawText("Yon tuslari veya WASD ile hareket et", 10, 10, 20, DARKGRAY);
+            DrawText("Sinirlar artik guvende!", 10, 40, 20, GREEN);
+
+        EndDrawing();
+    }
+
+    // --- 4. KAPANIŞ ---
+    UnloadTexture(frogImage); // Hafızayı boşalt (Mekatronik disiplini!)
+    CloseWindow();
 
     return 0;
-}
-
-// Update and draw game frame
-static void UpdateDrawFrame(void)
-{
-    // Update
-    //----------------------------------------------------------------------------------
-    UpdateCamera(&camera, CAMERA_ORBITAL);
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-
-            DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-            DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-            DrawGrid(10, 1.0f);
-
-        EndMode3D();
-
-        DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-
-        DrawFPS(10, 10);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
 }
